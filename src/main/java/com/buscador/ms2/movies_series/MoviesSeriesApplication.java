a5dc2e61e6d70980cc02205e6cd7ed1d05bb9d11
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-06-09 08:00:29 -0500

    first commit

---
+++ src/main/java/com/buscador/ms2/movies_series/MoviesSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.buscador.ms2.movies_series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSeriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buscador/ms2/movies_series/MoviesSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buscador.ms2.movies_series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
